8c59ce7232afe6b34c89414a729d49d5793a1149
Nikolaj Schmidke <niko.schmidke@googlemail.com>
2013-02-05 21:05:10 +0100

    hmm

--- core/src/main/java/de/bujan/core/Dame.java
+++ core/src/main/java/de/bujan/core/Dame.java
@@ -8,25 +8,29 @@ import static playn.core.PlayN.assets;
 import static playn.core.PlayN.graphics;
 
 public class Dame implements Game {
-  @Override
-  public void init() {
-    // create and add background image layer
-    Image bgImage = assets().getImage("images/bg.png");
-    ImageLayer bgLayer = graphics().createImageLayer(bgImage);
-    graphics().rootLayer().add(bgLayer);
-  }
+    @Override
+    public void init() {
+        // create and add background image layer
+        Image bgImage = assets().getImage("images/schachbrett.png");
 
-  @Override
-  public void paint(float alpha) {
-    // the background automatically paints itself, so no need to do anything here!
-  }
+        ImageLayer bgLayer = graphics().createImageLayer(bgImage);
+        //bgLayer.setHeight(bgImage.height());
+        //bgLayer.setWidth(bgImage.width());
+        //graphics().setSize((int)bgImage.height(), (int)bgImage.width());
+        graphics().rootLayer().add(bgLayer);
+    }
 
-  @Override
-  public void update(float delta) {
-  }
+    @Override
+    public void paint(float alpha) {
+        // the background automatically paints itself, so no need to do anything here!
+    }
 
-  @Override
-  public int updateRate() {
-    return 25;
-  }
+    @Override
+    public void update(float delta) {
+    }
+
+    @Override
+    public int updateRate() {
+        return 25;
+    }
 }
